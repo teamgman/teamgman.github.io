c1d3c03151b85d1afd733a1e6b6977845cf6ccc1
gman <109044072+teamgman@users.noreply.github.com>
2026-01-15 21:22:08 -0500

    Quartz sync: Jan 15, 2026, 9:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "teamgman.github.io",
+    baseUrl: "gavinxiong.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
