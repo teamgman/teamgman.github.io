11e1bddea16b30bd3d723c085dd3faddabb2ecf5
gman <109044072+teamgman@users.noreply.github.com>
2026-03-04 13:30:36 -0500

    Quartz sync: Mar 4, 2026, 1:30 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,7 +49,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta({showReadingTime: false})],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
