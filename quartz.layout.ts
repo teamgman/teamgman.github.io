235adfb3bc8f1f7682f100fcfec9e05efcac5b7d
gman <109044072+teamgman@users.noreply.github.com>
2026-03-04 12:28:55 -0500

    Quartz sync: Mar 4, 2026, 12:28 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,7 +20,9 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    Component.ContentMeta({
+      showReadingTime: false,
+    }),
     Component.TagList(),
   ],
   left: [
