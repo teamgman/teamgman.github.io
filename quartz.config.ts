abfe0604efb642777642acbb4938a2b4568dfe24
gman <109044072+teamgman@users.noreply.github.com>
2026-05-24 00:17:12 -0400

    Quartz sync: May 24, 2026, 12:17 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "gman",
+    pageTitle: "Gavin",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
