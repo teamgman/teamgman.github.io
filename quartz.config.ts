3f0a5371fbe76d40fd23eb25c22fbc594953a58a
gman <109044072+teamgman@users.noreply.github.com>
2026-05-24 00:15:39 -0400

    Quartz sync: May 24, 2026, 12:15 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents({ collapseByDefault: true }),
+      Plugin.TableOfContents({showByDefault: false}),
       Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
